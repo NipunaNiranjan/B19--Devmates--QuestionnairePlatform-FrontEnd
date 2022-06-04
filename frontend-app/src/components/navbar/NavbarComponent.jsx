9fb1ee3ebf5545af9c7c3f6185e6fb54979ccbaa
achintha <achinthathennakoon97@gmail.com>
2022-06-04 13:52:37 +0530

    completed basic structure of navbar

--- frontend-app/src/components/navbar/NavbarComponent.jsx
+++ frontend-app/src/components/navbar/NavbarComponent.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
+import "./NavbarComponent.css";
 
 function NavbarComponent() {
   return (
-    <Navbar bg="dark" variant="dark" sticky="top">
+    <Navbar bg="dark" variant="dark" sticky="top" expand="lg" collapseOnSelect>
       <Navbar.Brand>Home</Navbar.Brand>
-      <Nav.Link style={{ justifyContent: flexEnd }}>user</Nav.Link>
+
+      <NavbarToggle />
+      <Navbar.Collapse className="right-aligned">
+        <Nav>
+          <Nav.Link href="/logout">Log out</Nav.Link>
+          <NavDropdown align="end" title="user">
+            <NavDropdown.Item>Dashboard</NavDropdown.Item>
+            <NavDropdown.Item>Account</NavDropdown.Item>
+            <NavDropdown.Item>Grades</NavDropdown.Item>
+            <hr />
+            <NavDropdown.Item>Log out</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
