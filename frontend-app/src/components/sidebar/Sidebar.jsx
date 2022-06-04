3710911d1fbd75c65163e1fda9d7d5afeea56837
achintha <achinthathennakoon97@gmail.com>
2022-06-04 21:43:32 +0530

    created sidebar css file and set color

--- frontend-app/src/components/sidebar/Sidebar.jsx
+++ frontend-app/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
+import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <Navbar>
+    <Navbar bg="mySidebarColor">
       <Nav defaultActiveKey="/home" className="flex-column">
         <Nav.Link href="/home">home</Nav.Link>
         <Nav.Link eventKey="foo">foo</Nav.Link>
