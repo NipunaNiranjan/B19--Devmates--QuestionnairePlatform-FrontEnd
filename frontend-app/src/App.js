fe13a40e49030ce7f9d90c987e72dd28c089f86c
achintha <achinthathennakoon97@gmail.com>
2022-06-04 21:26:26 +0530

    initialized sidebar component

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import NavbarComponent from "./components/navbar/NavbarComponent";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
-      <h1>App component</h1>
+      <Sidebar />
     </div>
   );
 }
