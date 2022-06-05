c0344ccf80740e5c4c00c265866710bbce7127f1
achintha <achinthathennakoon97@gmail.com>
2022-06-05 09:37:49 +0530

    Deleted sidebar structure

--- frontend-app/src/components/sidebar/Sidebar.jsx
+++ frontend-app/src/components/sidebar/Sidebar.jsx
@@ -4,16 +4,7 @@ import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <Navbar bg="mySidebarColor">
-      <Nav defaultActiveKey="/home" className="flex-column">
-        <Nav.Link href="/home">home</Nav.Link>
-        <Nav.Link eventKey="foo">foo</Nav.Link>
-        <Nav.Link eventKey="bar">bar</Nav.Link>
-        <Nav.Link eventKey="disabled" disabled>
-          disabled
-        </Nav.Link>
-      </Nav>
-    </Navbar>
+
   );
 }
 
