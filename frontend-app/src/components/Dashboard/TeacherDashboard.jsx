78faa7b5f1191a077ec8058a02ceb7d6d7e4ce64
achintha <achinthathennakoon97@gmail.com>
2022-06-09 15:47:20 +0530

    Added two buttons to switch form

--- frontend-app/src/components/Dashboard/TeacherDashboard.jsx
+++ frontend-app/src/components/Dashboard/TeacherDashboard.jsx
@@ -1,25 +1,36 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+import CreateClass from "./createClassOrganization/CreateClass";
+import CreateOrganization from "./createClassOrganization/CreateOrganization";
 
 function TeacherDashboard() {
-  const [createClassButton, setCreateclassButton] = useState(true);
+  const [createClassButton, setCreateclassButton] = useState(false);
 
-  const handleButton1 = () => {
-    setCreateclassButton = true;
-  };
+  // const handleButton1 = () => {
+  //   setCreateclassButton = true;
+  // };
 
-  const handleButton2 = () => {
-    setCreateclassButton = false;
-  };
+  // const handleButton2 = () => {
+  //   setCreateclassButton = false;
+  // };
 
   return (
     <div>
-      <Button variant="primary" size="lg" onClick={handleButton1}>
-        Large button
+      <Button
+        variant="primary"
+        size="lg"
+        onClick={() => setCreateclassButton(true)}
+      >
+        Craete class
       </Button>{" "}
-      <Button variant="secondary" size="lg" onClick={handleButton2}>
-        Large button
+      <Button
+        variant="secondary"
+        size="lg"
+        onClick={() => setCreateclassButton(false)}
+      >
+        Create Organization
       </Button>
+      <div>{createClassButton ? <CreateClass /> : <CreateOrganization />}</div>
     </div>
   );
 }
