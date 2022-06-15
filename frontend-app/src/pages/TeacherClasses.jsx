4aa741b8cd707d496957cb3c9c7f9b1e57b58e37
achintha <achinthathennakoon97@gmail.com>
2022-06-15 08:23:17 +0530

    import nav bar and sidebar

--- frontend-app/src/pages/TeacherClasses.jsx
+++ frontend-app/src/pages/TeacherClasses.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import NavbarComponent from "../components/navbar/NavbarComponent";
+import Sidebar from "../components/sidebar/Sidebar";
 
 function TeacherClasses() {
   return (
