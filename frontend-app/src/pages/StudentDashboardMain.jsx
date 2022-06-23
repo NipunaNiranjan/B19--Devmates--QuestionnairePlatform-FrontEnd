1d6b30848aa407b4d07dad7a656563bba12fbc9b
achintha <achinthathennakoon97@gmail.com>
2022-06-23 09:29:11 +0530

    mapped student classes in student dashboard

--- frontend-app/src/pages/StudentDashboardMain.jsx
+++ frontend-app/src/pages/StudentDashboardMain.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import NavbarComponent from "../components/navbar/NavbarComponent";
 import Sidebar from "../components/sidebar/Sidebar";
 
@@ -11,8 +12,9 @@ function StudentDashboardMain() {
   }, []);
 
   function refreshClasses() {
-    const ProjectAPI = axios
-      .get("api/v1/class/student/4")
+    const sid = localStorage.getItem("userID");
+    axios
+      .get(`api/v1/class/student/${sid}`)
       .then((res) => {
         setClasses(res.data.body);
         console.log(res.data.body);
@@ -30,26 +32,27 @@ function StudentDashboardMain() {
 
         <div className="container cont_style p-3 mb-5 mt-3  text-dark justify-content-center align-items-center">
           <div className="row justify-content-center m-5 ">
-            <div className="col-6 ">
-              <div className="card m-5">
-                <div className="card-body b_style">
-                  <h5 className="card-title">Class Name</h5>
-                </div>
-                <div className="card-footer fStyle">
-                  <small className="text-muted">Teacher's Name</small>
+            {classes.map((item) => (
+              <div className="col-6 ">
+                <div className="card m-5 ">
+                  <div className="card-body b_style">
+                    <h5 className="card-title ">{item.className}</h5>
+                  </div>
+                  <div className="card-footer  fStyle">
+                    <small className="text-muted">
+                      {" "}
+                      <Link
+                        to={
+                          "/dashboard/teacher/addStudentClass/" + item.classId
+                        }
+                      >
+                        Click to enter class-{">"}
+                      </Link>{" "}
+                    </small>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-6 ">
-              <div className="card m-5 ">
-                <div className="card-body b_style">
-                  <h5 className="card-title ">Class Name</h5>
-                </div>
-                <div className="card-footer  fStyle">
-                  <small className="text-muted">Teacher's Name</small>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
