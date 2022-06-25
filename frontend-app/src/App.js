6c99217012889143e20b40ca02d8de59bd70cafc
achintha <achinthathennakoon97@gmail.com>
2022-06-25 20:25:31 +0530

    added new routes

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,59 +1,57 @@
-import axios from "axios";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import AdminDashboardViewClasses from "./pages/AdminDashboardViewClasses";
 import AdminDashboardViewUsers from "./pages/AdminDashboardViewUsers";
 import TeacherClasses from "./pages/TeacherClasses";
 import TeachersDashboard from "./pages/TeachersDashboard";
-import Login from "./components/Login";
 import AdminDashboardMain from "./pages/AdminDashboardMain";
 import StudentDashboardMain from "./pages/StudentDashboardMain";
 import AddStudents from "./pages/AddStudents";
+import Login from "./components/forms/Login";
+import { HomePage } from "./pages/Home/HomePage";
 import Class from "./pages/Class";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route
-          exact
-          path="/dashboard/admin/viewusers"
-          element={<AdminDashboardViewUsers />}
-        />
-        <Route
-          exact
-          path="/dashboard/admin/viewClasses"
-          element={<AdminDashboardViewClasses />}
-        />
-        <Route
-          exact
-          path="/dashboard/teacher"
-          element={<TeachersDashboard />}
-        />
-        <Route
-          exact
-          path="/dashboard/teacher/viewClasses"
-          element={<TeacherClasses />}
-        />
-        <Route exact path="/dashboard/admin" element={<AdminDashboardMain />} />
-        <Route
-          exact
-          path="/dashboard/student"
-          element={<StudentDashboardMain />}
-        />
-        <Route
-          exact
-          path="/dashboard/teacher/addStudentClass/:classId/:className"
-          element={<AddStudents />}
-        />
-        <Route
-          exact
-          path="/dashboard/class/:classId/:className"
-          element={<Class />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />
+      <Route exact path="/login" element={<Login />} />
+      {/* Admin routes */}
+      <Route
+        exact
+        path="/dashboard/admin/viewusers"
+        element={<AdminDashboardViewUsers />}
+      />
+      <Route exact path="/dashboard/admin" element={<AdminDashboardMain />} />
+      <Route
+        exact
+        path="/dashboard/admin/viewClasses"
+        element={<AdminDashboardViewClasses />}
+      />
+      {/* Teacher */}
+      <Route exact path="/dashboard/teacher" element={<TeachersDashboard />} />
+      <Route
+        exact
+        path="/dashboard/teacher/viewClasses"
+        element={<TeacherClasses />}
+      />
+      {/* student */}
+      <Route
+        exact
+        path="/dashboard/student"
+        element={<StudentDashboardMain />}
+      />
+      <Route
+        exact
+        path="/dashboard/teacher/addStudentClass/:classId/:className"
+        element={<AddStudents />}
+      />
+      <Route
+        exact
+        path="/dashboard/class/:classId/:className"
+        element={<Class />}
+      />
+    </Routes>
   );
 }
 
